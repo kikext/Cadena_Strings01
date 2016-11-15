f58ae130521d36c4a3bf98e084fba643e068fd42
kikext <e-imbert@hotmail.com>
2016-11-15 17:15:00 +0100

    New Ex

---
+++ src/com/company/EX12.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * Created by 25374183P on 15/11/2016.
+ */
+public class EX12 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Dame un DNI y comprobaré su validez");
+            String dni = scanner.nextLine();
+        for (int i=0; i<dni.length();i++){
+
+        }
+
+    }
+}
